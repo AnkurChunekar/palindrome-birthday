a2c9a92de93a8ae517d280370b2ba830621eb928
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2021-09-08 13:14:12 -0700

    Feat: function for all formats

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const string = "good";
+const date = {
+  day: 6,
+  month: 2,
+  year: 2020
+}
 
 function reverseString(str) {
   const strSplit = str.split("");
@@ -15,3 +19,56 @@ function checkPalindrome(stringOne) {
     return false;
   }
 }
+
+function convertDateToString(date) {
+ 
+var day = date.day;
+var month = date.month;
+var year = date.year;
+
+if (day > 9 ) {
+day = date.day.toString();
+} else { 
+day = "0" + date.day;
+}
+
+if ( month > 9) {
+month = month.toString();
+}  else {
+  month = "0" + month;
+}
+
+
+year = year.toString();
+
+date = {
+  day: day,
+  month: month,
+  year: year,
+}
+
+return date;
+}
+
+
+var newdate = convertDateToString(date);
+
+
+function convertDateToAllFormats(date) {
+// DD-MM-YYYY
+// MM-DD-YYYY
+// YYYY-MM-DD
+// DD-MM-YY
+// MM-DD-YY
+// YY-MM-DD
+const ddmmyyyy = date.day + date.month + date.year;
+const mmddyyyy = date.month + date.day + date.year;
+const yyyymmdd = date.year + date.month + date.day;
+const ddmmyy = date.day + date.month + date.year.slice(-2);
+const mmddyy=  date.month + date.day + date.year.slice(-2);
+const yymmdd = date.year.slice(-2) + date.month + date.day;
+
+const array = [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
+return array;
+}
+
